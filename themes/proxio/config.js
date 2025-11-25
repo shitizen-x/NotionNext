2cf180b55673f8145a7a82b4b577317473570e29
shitizen-x <wdsmzx@gmail.com>
2025-11-25 12:18:01 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -3,19 +3,18 @@
  */
 const CONFIG = {
   PROXIO_WELCOME_COVER_ENABLE: true, //是否显示页面进入的欢迎文字
-  PROXIO_WELCOME_TEXT: '欢迎来到此网站，点击任意位置进入', // 欢迎文字，留空则不启用
+  PROXIO_WELCOME_TEXT: '欢迎来到LPRD，点击任意位置进入', // 欢迎文字，留空则不启用
 
   // 英雄区块导航
   PROXIO_HERO_ENABLE: true, // 开启英雄区
-  PROXIO_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-  PROXIO_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  PROXIO_HERO_TITLE_1: '作为市场与文化的桥梁，将宏伟蓝图转化为标志性现实', // 英雄区文字
+  PROXIO_HERO_TITLE_2: '并为开发项目直接对接庞大的中国投资群体，提供跨越项目全生命周期的整合服务,成倍放大您的价值', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  PROXIO_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
-  PROXIO_HERO_BUTTON_1_URL:
-    'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github-mark.svg', // 英雄区按钮2的图标，不需要则留空
+  PROXIO_HERO_BUTTON_1_TEXT: '查看案例', // 英雄区按钮
+  PROXIO_HERO_BUTTON_1_URL: '#', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_TEXT: '联系我们', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_URL: '#', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
   PROXIO_HERO_BANNER_IMAGE: '', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
@@ -37,8 +36,8 @@ const CONFIG = {
 
   // 特性区块
   PROXIO_FEATURE_ENABLE: true, // 特性区块开关
-  PROXIO_FEATURE_TITLE: '为什么选我',
-  PROXIO_FEATURE_TEXT_1: '我能让您的项目焕发光彩',
+  PROXIO_FEATURE_TITLE: '为什么选我们',
+  PROXIO_FEATURE_TEXT_1: '我们能让您的项目焕发光彩',
   PROXIO_FEATURE_TEXT_2: '丰富的案例经验，专业的技术服务，优质的沟通效率',
 
   // 特性1
@@ -60,147 +59,152 @@ const CONFIG = {
     '精益求精雕琢每个元素，确保成品精致统一，令人过目难忘',
 
   PROXIO_FEATURE_BUTTON_TEXT: '了解更多', // 按钮文字
-  PROXIO_FEATURE_BUTTON_URL: 'https://github.com/tangly1024/NotionNext', // 按钮跳转
+  PROXIO_FEATURE_BUTTON_URL: '#', // 按钮跳转
 
   // 首页生涯区块
   PROXIO_CAREER_ENABLE: true, // 区块开关
-  PROXIO_CAREER_TITLE: '生涯',
-  PROXIO_CAREER_TEXT: '以下是我的职业生涯',
+  PROXIO_CAREER_TITLE: '生态',
+  PROXIO_CAREER_TEXT: '以下是我们的生态',
 
   // 生涯内容卡牌 ，title是标题 ，bio是备注，text是详情
   PROXIO_CAREERS: [
     {
-      title: 'Freelance Architect',
-      bio: '2016-2020',
-      text: 'As a freelance architect, I worked on a range of residential and commercial projects, balancing form and function. I collaborated with clients and contractors to transform concepts into reality, ensuring each design was both aesthetically pleasing and structurally sound.'
+      title: '室内设计公司',
+      bio: '梦想的蓝图师',
+      text: '这里是灵感成型的起点。我们倾听您的愿景，将其转化为兼具美学灵性与实用功能的空间蓝图，让梦想跃然纸上，清晰可触。'
     },
     {
-      title: 'Product Designer at Spotify',
-      bio: '2020-2022',
-      text: 'At Spotify, I helped shape innovative features for millions of users globally. My focus was on creating seamless music discovery experiences, enhancing user interfaces, and optimizing cross-platform navigation, which led to an improved product flow and increased user satisfaction.'
+      title: '建筑模型工作室',
+      bio: '未来的预见者',
+      text: '在破土动工之前，赋予设计以生命。我们的精密建筑模型将蓝图化为直观的三维实体，让您亲见未来，确保每一个决策都精准无误。'
     },
     {
-      title: 'Freelance Product Designer',
-      bio: '2022-Now',
-      text: 'Now I design user-centric products that align with client visions. As a freelance product designer, I collaborate with startups and established companies, crafting solutions that elevate user experiences and increase engagement across both digital and physical interfaces.'
+      title: '装饰工程公司',
+      bio: '承诺的守护者',
+      text: '我们是理念的卓越执行者。凭借精湛的工艺与严格的流程管理，我们守护着您的梦想、预算与工期，确保每一张设计图纸都被完美复刻为现实。'
+    },
+    {
+      title: '定制家居品牌',
+      bio: '个性的雕刻家',
+      text: '空间的真正个性，藏于独一无二的细节之中。我们为您量身定制每一件家具与软饰，以画龙点睛之笔，为空间注入专属的性格与温度。'
     }
   ],
 
   // 首页用户测评区块
   PROXIO_TESTIMONIALS_ENABLE: true, // 测评区块开关
-  PROXIO_TESTIMONIALS_TITLE: '用户反馈',
-  PROXIO_TESTIMONIALS_TEXT_1: '我们的用户怎么说',
+  PROXIO_TESTIMONIALS_TITLE: '管理层',
+  PROXIO_TESTIMONIALS_TEXT_1: '我们的团队',
   PROXIO_TESTIMONIALS_TEXT_2:
-    '数千位站长选择用NotionNext搭建他们的网站,通过帮助手册、交流社群以及技术咨询，大家成功上线了自己的网站',
+    '团队简介，团队简介，团队简介，团队简介，团队简介，团队简介，团队简介，团队简介，团队简介，',
 
   // 用户测评处的跳转按钮
   PROXIO_TESTIMONIALS_BUTTON_URL: '/about',
-  PROXIO_TESTIMONIALS_BUTTON_TEXT: '联系我',
+  PROXIO_TESTIMONIALS_BUTTON_TEXT: '联系我们',
 
   // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
   PROXIO_TESTIMONIALS_ITEMS: [
     {
       PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ ',
+        '人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，',
       PROXIO_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F22de3fcb-d90d-4271-bc01-f815f476122b%2F4FE0A0C0-E487-4C74-BF8E-6F01A27461B8-14186-000008094BC289A6.jpg?table=collection&id=a320a2cc-6ebe-4a8d-95cc-ea94e63bced9&width=200',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Ryan_G',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Ryan`Log 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://blog.gaoran.xyz/'
+      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '名字',
+      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '设计师',
+      PROXIO_TESTIMONIALS_ITEM_URL: '#'
     },
     {
       PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '很喜欢这个主题，本代码小白用三天台风假期搭建出来了，还根据大佬的教程弄了自定义域名，十分感谢，已请喝咖啡~',
+        '人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，',
       PROXIO_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d33d169-f932-41ff-ac6b-88a923c08e02%2F%25E5%25A4%25B4%25E5%2583%258F.jfif?table=collection&id=7787658d-d5c0-4f34-8e32-60c523dfaba3&width=400',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Asenkits',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '阿森的百宝袋 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://asenkits.top/'
+      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '名字',
+      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '设计师',
+      PROXIO_TESTIMONIALS_ITEM_URL: '#'
     },
     {
       PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '呜呜呜，经过一个下午的努力，终于把博客部署好啦，非常感谢Tangly1024大佬的框架和教程，这是我有生之年用过的最好用的博客框架┭┮﹏┭┮。从今之后，我就可以在自己的博客里bb啦，( •̀ ω •́ )y ',
+        '人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，',
       PROXIO_TESTIMONIALS_ITEM_AVATAR: '/avatar.png',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'DWIND',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '且听风吟 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.dwind.top/'
+      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '名字',
+      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '业务总监',
+      PROXIO_TESTIMONIALS_ITEM_URL: '#'
     },
     {
       PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '感谢提供这么好的项目哈哈 之前一直不知道怎么部署(别的项目好难好复杂)这个相对非常简单 新手非常友好哦',
+        '人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，',
       PROXIO_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd52f6766-3e32-4c3d-8529-46e1f214360f%2Ffavicon.svg?table=collection&id=7d76aad5-a2c4-4d9a-887c-c7913fae4eed&width=400',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '迪升disheng ',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'AI资源分享 Blog',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://blog.disheng.org/'
+      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '名字',
+      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '营销总监',
+      PROXIO_TESTIMONIALS_ITEM_URL: '#'
     },
     {
       PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '灰常感谢大佬的博客项目，能将博客和notion结合起来，这一直是我挺期待的博客模式。',
+        '人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，',
       PROXIO_TESTIMONIALS_ITEM_AVATAR: '/avatar.png',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'AnJhon',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Anjhon`s Blog 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.anjhon.top'
+      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '名字',
+      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '全球市场',
+      PROXIO_TESTIMONIALS_ITEM_URL: '#'
     },
     {
-      PROXIO_TESTIMONIALS_ITEM_TEXT: '用好久了，太感谢了',
+      PROXIO_TESTIMONIALS_ITEM_TEXT: '人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，人员介绍，',
       PROXIO_TESTIMONIALS_ITEM_AVATAR: '/avatar.png',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'LUCEN',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'LUCEN考验辅导 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.lucenczz.top/'
+      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '名字',
+      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'CEO',
+      PROXIO_TESTIMONIALS_ITEM_URL: '#'
     }
   ],
 
   //   FAQ 常见问题模块
   PROXIO_FAQ_ENABLE: true, // 常见问题模块开关
-  PROXIO_FAQ_TITLE: '常见问题解答',
+  PROXIO_FAQ_TITLE: '常见问题0解答',
   PROXIO_FAQ_TEXT_1: '有任何问题吗？请看这里',
   PROXIO_FAQ_TEXT_2: '我们收集了常见的用户疑问',
   PROXIO_FAQS: [
     {
-      q: 'NotionNext有帮助文档吗？',
-      a: 'NotionNext提供了<a href="https://docs.tangly1024.com/about" className="underline">帮助文档</a>，操作<a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">演示视频</a>，以及<a href="https://docs.tangly1024.com/article/chat-community" className="underline">交流社群</a>来协助您完成网站的搭建部署'
+      q: '常见问题1',
+      a: '常见问题1常见问题1常见问题1常见问题1常见问题1常见问题1'
     },
     {
-      q: '部署后要如何编写文章？',
-      a: '您可以在Notion中之间添加或修改类型为Post的页面，内容将被实时同步在站点中，详情参考<a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《帮助文档》</a>'
+      q: '常见问题2',
+      a: '常见问题2常见问题2常见问题2常见问题2常见问题2常见问题2'
     },
     {
-      q: '站点部署失败，更新失败？',
-      a: '通常是配置修改错误导致，请检查配置或者重试操作步骤，或者通过Vercel后台的Deployments中找到错误日志，并向网友求助'
+      q: '常见问题3',
+      a: '常见问题3常见问题3常见问题3常见问题3常见问题3常见问题3'
     },
     {
-      q: '文章没有实时同步？',
-      a: '先检查Notion_Page_ID是否正确配置，其次由于博客的每个页面都有独立缓存，刷新网页后即可解决'
+      q: '常见问题4',
+      a: '常见问题4常见问题4常见问题4常见问题4常见问题4常见问题4'
     }
   ],
 
   // 关于作者区块
   PROXIO_ABOUT_ENABLE: true, // 关于作者区块区块开关
-  PROXIO_ABOUT_TITLE: '关于作者',
-  PROXIO_ABOUT_TEXT_1: 'I am an Architect Turned Into a Product Designer',
+  PROXIO_ABOUT_TITLE: '关于我们',
+  PROXIO_ABOUT_TEXT_1: 'LPRD是一家以"全球整合，本地臻誉"为核心理念的集团公司',
   PROXIO_ABOUT_TEXT_2:
-    'With a background in architecture, I now apply my expertise to product design, blending aesthetics, functionality, and innovation. My goal is to create modern, user-focused designs that bring your vision to life.',
-  PROXIO_ABOUT_PHOTO_URL: '/avatar.png',
+    '在中国波澜壮阔的城市化进程中砥砺前行三十年。如今，我们将这一愿景带至全球，提供跨越项目全生命周期的整合服务。我们的使命是作为市场与文化的桥梁，将宏伟蓝图转化为标志性现实，并为开发项目直接对接庞大的中国投资群体。我们志在超越传统服务商的角色，成为驱动可持续增长的战略合作伙伴，建立历久弥坚的合作关系，共同促进地区繁荣与和谐发展。',
+  PROXIO_ABOUT_PHOTO_URL: 'https://img.181024.xyz/file/1764043471271_%E5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20251125120337.png',
   PROXIO_ABOUT_KEY_1: '经验年限',
   PROXIO_ABOUT_VAL_1: '10年+',
   PROXIO_ABOUT_KEY_2: '客户',
   PROXIO_ABOUT_VAL_2: '1000+',
   PROXIO_ABOUT_KEY_3: '交付项目',
   PROXIO_ABOUT_VAL_3: '5000+',
-  PROXIO_ABOUT_KEY_4: '累积创作时长（小时）',
-  PROXIO_ABOUT_VAL_4: '10000+',
+  PROXIO_ABOUT_KEY_4: '服务范围',
+  PROXIO_ABOUT_VAL_4: '全球',
 
   PROXIO_ABOUT_BUTTON_URL: '/about',
-  PROXIO_ABOUT_BUTTON_TEXT: '关于我',
+  PROXIO_ABOUT_BUTTON_TEXT: '关于我们',
 
   // 横向滚动文字
   PROXIO_BRANDS_ENABLE: true, // 滚动文字
   PROXIO_BRANDS: [
-    'Web Design',
-    'Logo Design',
-    'Mobile App Design',
-    'Product Design'
+    'Rain Design Co',
+    'Le Prime Model Co',
+    'Decoration Engineering Co',
+    'Custom Furnishings Brand'
   ],
 
   PROXIO_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
@@ -209,37 +213,25 @@ const CONFIG = {
   // 页脚菜单
   PROXIO_FOOTER_LINKS: [
     {
-      name: '友情链接',
+      name: '其他信息',
       menus: [
         {
-          title: 'Tangly的学习笔记',
-          href: 'https://blog.tangly1024.com'
+          title: '其他信息',
+          href: '#'
         },
         {
-          title: 'NotionNext',
-          href: 'https://www.tangly1024.com'
+          title: '其他信息',
+          href: '#'
         }
       ]
     },
     {
-      name: '开发者',
+      name: '其他信息',
       menus: [
-        { title: 'Github', href: 'https://github.com/tangly1024/NotionNext' },
+        { title: '其他信息', href: '#' },
         {
-          title: '开发帮助',
-          href: 'https://docs.tangly1024.com/article/how-to-develop-with-notion-next'
-        },
-        {
-          title: '功能反馈',
-          href: 'https://github.com/tangly1024/NotionNext/issues/new/choose'
-        },
-        {
-          title: '技术讨论',
-          href: 'https://github.com/tangly1024/NotionNext/discussions'
-        },
-        {
-          title: '关于作者',
-          href: 'https://blog.tangly1024.com/about'
+          title: '其他信息',
+          href: '#'
         }
       ]
     }
@@ -263,16 +255,16 @@ const CONFIG = {
 
   // 页面底部的行动呼吁模块
   PROXIO_CTA_ENABLE: true,
-  PROXIO_CTA_TITLE: '与我建立联系',
+  PROXIO_CTA_TITLE: '与我们建立联系',
   PROXIO_CTA_TITLE_2: '让我们立刻启动您的项目',
   PROXIO_CTA_DESCRIPTION:
-    '访问NotionNext的操作文档，我们提供了详细的教程，帮助你即刻搭建站点',
+    '我们志在超越传统服务商的角色，成为驱动可持续增长的战略合作伙伴，建立历久弥坚的合作关系，共同促进地区繁荣与和谐发展。',
   PROXIO_CTA_BUTTON: true, // 是否显示按钮
   PROXIO_CTA_BUTTON_URL: '/about',
-  PROXIO_CTA_BUTTON_TEXT: '联系我',
+  PROXIO_CTA_BUTTON_TEXT: '联系我们',
 
   PROXIO_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
-  PROXIO_POST_REDIRECT_URL: 'https://blog.tangly1024.com', // 重定向域名
+  PROXIO_POST_REDIRECT_URL: '#', // 重定向域名
   PROXIO_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_PROXIO_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
 export default CONFIG
